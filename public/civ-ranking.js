fe8e27981933fd11f5d43c4077b192f5d75f559b
Peter White <pedroblanco3257@gmail.com>
2020-11-18 13:50:53 -0800

    Finished off the first 10 civs.

--- public/civ-ranking.js
+++ public/civ-ranking.js
@@ -23,56 +23,67 @@ $.getJSON('data.json', function(data) {
             for(let j = 0; j < units.length; j++){
                 let unit = units[j];
                 let ranks = civ.ranks[buildings[i]][units[j]];
-                ranks = ranks.map((r) => {
+                let rankValues = [];
+                ranks = ranks.map((r, k) => {
                     buildingRanks.push(r);
+                    if(r > 3) rankValues.push("high");
+                    else if(r < 2) rankValues.push("low");
+                    else rankValue = rankValues.push("medium");
                     return data.rankConversion[(r + "")];
                 });
                 while(ranks.length < 4){
-                    ranks.unshift("-");
+                    ranks.unshift(" ");
+                    rankValues.unshift("medium");
                 }
                 let unitdiv = $("<div class='unit-div'></div>");
                 unitdiv.append("<div class='unit'>"+"<img src='img/"+unit+".png'>"+"</div>");
-                ranks.forEach((r) => {
-                    unitdiv.append("<div class='rank'>"+r+"</div>");
+                ranks.forEach((r, k) => {
+                    let str = r;
+                    if(str.length === 2) str = "&nbsp" + str;
+                    let rankDiv = $("<div class='rank "+(rankValues[k] + "-rank")+"'>"+str+"</div>");
+                    unitdiv.append(rankDiv);
                 });
                 unitrow.append(unitdiv);
             }
             let numRanks = buildingRanks.length;
             //Produce a number that ends in 0 or 0.5
             let avgRank = Math.round(buildingRanks.reduce((a, b) => a + b, 0) / numRanks * 2) / 2;
-            row.children(".building:last-child").append("<div class='building-rank'>"+data.rankConversion[(avgRank + "")]+"</div>");
+            let rankStr = data.rankConversion[(avgRank + "")];
+            if(rankStr.length === 2) rankStr = "&nbsp" + rankStr;
+            else rankStr = "&nbsp" + rankStr + "&nbsp";
+            row.children(".building:last-child").append("<div class='building-rank'><div>"+rankStr+"</div></div>");
             
             row.append(unitrow);
             $("#tree").append(row);
         }
         //Adjust the size of the images (which reduces the size of the rows) FOR PRINTING ONLY.
         let unitNum = $(".unit").length;
-        if(unitNum === 21){
+        if(unitNum === 22){
             $(".age img").addClass("xx-small-img");
             $(".building img").addClass("xx-small-img");
             $(".unit img").addClass("xx-small-img");
             $(".age-spacer img").addClass("xx-small-img");
             $(".ages-cont img").addClass("xx-small-img");
-        } else if(unitNum === 20){
+        } else if(unitNum === 21){
             $(".age img").addClass("xa-small-img");
             $(".building img").addClass("x-small-img");
             $(".unit img").addClass("xa-small-img");
             $(".age-spacer img").addClass("x-small-img");
             $(".ages-cont img").addClass("x-small-img");
-        } else if(unitNum >= 18){
+        } else if(unitNum >= 19){
             $(".age img").addClass("x-small-img");
             $(".building img").addClass("x-small-img");
             $(".unit img").addClass("x-small-img");
             $(".age-spacer img").addClass("x-small-img");
             $(".ages-cont img").addClass("x-small-img");
-        } else if(unitNum >= 16){
+        } else if(unitNum >= 17){
             $(".age img").addClass("small-img");
             $(".building img").addClass("small-img");
             $(".unit img").addClass("small-img");
             $(".age-spacer img").addClass("small-img");
             $(".ages-cont img").addClass("small-img");
         }
-        
+        //console.log(unitNum)
         $("#civ-bonuses").children(".bonus-desc").empty();
         $("#civ-available-techs").empty();
         //Show tech information
@@ -112,7 +123,7 @@ $.getJSON('data.json', function(data) {
             ["thumb ring", "parthian tactics"],
             ["bloodlines", "husbandry"],
             ["sanctity", "redemption", "atonement", "illumination", "block printing"],
-            ["siege engineers", "masonry", "fortified wall", "treadmill crane", "sappers", "arrowslits", "guard tower","bombard tower upgrade", "hoardings"]
+            ["siege engineers", "masonry", "fortified wall", "hoardings", "sappers", "arrowslits", "guard tower","bombard tower upgrade"]
         ];
         if(finder(civ.techTree.university.upgrades, "architecture").available === true) ups[4][ups[4].indexOf("masonry")] = "architecture";
         if(finder(civ.techTree.university.upgrades, "keep").available === true) ups[4][ups[4].indexOf("guard tower")] = "keep";
@@ -157,17 +168,20 @@ $.getJSON('data.json', function(data) {
             $("#civ-available-techs").append(row);
         });
         
-        if(descTextSize > 800){
+        if(descTextSize > 950){
             $("#civ-bonuses").children(".bonus-desc").children("p").addClass("x-small-desc-text");
             $(".desc-img img").addClass("x-small-img");
-        } else if(descTextSize > 700){
+        } else if(descTextSize > 800){
             $("#civ-bonuses").children(".bonus-desc").children("p").addClass("x-small-desc-text");
-        } else if(descTextSize > 600){
+        } else if(descTextSize > 750){
+            $("#civ-bonuses").children(".bonus-desc").children("p").addClass("small-desc-text");
+            $(".desc-img img").addClass("x-small-img");
+        } else if(descTextSize > 700){
             $("#civ-bonuses").children(".bonus-desc").children("p").addClass("small-desc-text");
         } 
-        //console.log(descTextSize);
+        console.log(descTextSize);
     });
-    $(".civ-cont:eq(2)").trigger("click");
+    $(".civ-cont:eq(9)").trigger("click");
     
 });
 });
