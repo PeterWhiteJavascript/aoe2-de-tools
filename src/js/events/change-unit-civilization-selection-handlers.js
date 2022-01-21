7765df307deedab6c252e1636039bb7243fd9a2f
Daniel Rafaj <daniel.rafaj@tuta.io>
2022-01-21 11:54:18 +0100

    copy implementation to make it easier to figure out what was before there

--- src/js/events/change-unit-civilization-selection-handlers.js
+++ src/js/events/change-unit-civilization-selection-handlers.js
@@ -93,6 +93,68 @@ export const calculateCivilizationBonusOnUnit = (target) => {
   return { tempTrainTime, priceChanged }
 }
 
+export const calculateCivilizationBonusOnUnit2 = (target) => {
+  const unitBox = target.closest('.unit-container[x-unit]')
+  const unit = unitBox.getAttribute('x-unit')
+
+  const unitStatsBox = document.querySelector(`[unit="${unit}"]`)
+  const setUnitResourceAttributes = setResAttributeCiv(unitStatsBox)
+  const setResourceToBaseValue = setResToBaseValue(unitStatsBox)
+
+  const baseTraintime = parseFloat(
+    unitStatsBox.getAttribute('x-base-train-time')
+  )
+  let tempTrainTime = baseTraintime
+  let priceChanged = false
+
+  const selectElem = document.querySelector(
+    `#gather-rates .unit-container[x-unit="${unit}"] select[unit-option="civilization"]`
+  )
+  let it = selectElem.querySelector(`option[value="${selectElem.value}"]`) // option element with data
+  if (!it) it = selectElem.firstElementChild // take Generic option if there is no other option selected
+  const trainTime = it.getAttribute('x-opt-train-time')
+  const percent = it.getAttribute('x-opt-train-time-percent')
+  if (trainTime) {
+    if (percent && percent === 'true') {
+      tempTrainTime = tempTrainTime / parseFloat(trainTime)
+    } else {
+      tempTrainTime = parseFloat(trainTime)
+    }
+  }
+
+  const food = it.getAttribute('x-opt-cost-food')
+  const wood = it.getAttribute('x-opt-cost-wood')
+  const gold = it.getAttribute('x-opt-cost-gold')
+  const stone = it.getAttribute('x-opt-cost-stone')
+
+  // modify x-food or reset it to base-value for later computation
+  if (food) {
+    priceChanged = true
+    setUnitResourceAttributes({ type: 'food', res: food })
+  } else {
+    setResourceToBaseValue('food')
+  }
+  if (wood) {
+    priceChanged = true
+    setUnitResourceAttributes({ type: 'wood', res: wood })
+  } else {
+    setResourceToBaseValue('wood')
+  }
+  if (gold) {
+    priceChanged = true
+    setUnitResourceAttributes({ type: 'gold', res: gold })
+  } else {
+    setResourceToBaseValue('gold')
+  }
+  if (stone) {
+    priceChanged = true
+    setUnitResourceAttributes({ type: 'stone', res: stone })
+  } else {
+    setResourceToBaseValue('stone')
+  }
+  return { tempTrainTime, priceChanged }
+}
+
 export const unitCivChangeSelectionHandlers = (event) => {
   if (!event.target.matches('[unit-option="civilization"]')) return
   let option = event.target.querySelector(`[value="${event.target.value}"]`)
--- src/js/events/click-unit-bonuses-handlers.js
+++ src/js/events/click-unit-bonuses-handlers.js
@@ -100,8 +100,76 @@ const updateUnitResourceInfo = (unitInfoFrom, unitInfoTo) => {
   }
   fwgs.map(updateResource)
 }
-
 export const calculateBonusesOnUnit = (option) => (target) => {
+  let { priceChanged, tempTrainTime } = Object.assign(
+    {
+      priceChanged: false,
+    },
+    option // override defaults
+  )
+
+  const unitBox = target.closest('.unit-container[x-unit]')
+  const unit = unitBox.getAttribute('x-unit')
+  const unitStatsBox = document.querySelector(`[unit="${unit}"]`)
+  // set defaults
+
+  const setUnitResourceAttributes = setResAttribute(unitStatsBox)
+  const setResourceToBaseValue = setResToBaseValue(unitStatsBox)
+
+  swapImageIfPresent({ input: target, unit })
+
+  Array.from(
+    document.querySelectorAll(
+      `#gather-rates .unit-container[x-unit="${unit}"] input:checked`
+    )
+  ).map((it) => {
+    const trainTime = it.getAttribute('x-train-time')
+    const percent = it.getAttribute('x-train-time-percent')
+    if (trainTime) {
+      if (percent && percent === 'true') {
+        tempTrainTime = tempTrainTime / parseFloat(trainTime)
+      } else {
+        tempTrainTime = parseFloat(trainTime)
+      }
+    }
+
+    const food = it.getAttribute('x-cost-food')
+    const wood = it.getAttribute('x-cost-wood')
+    const gold = it.getAttribute('x-cost-gold')
+    const stone = it.getAttribute('x-cost-stone')
+    if (food) {
+      priceChanged = true
+      setUnitResourceAttributes({ type: 'food', res: food })
+    }
+    if (wood) {
+      priceChanged = true
+      setUnitResourceAttributes({ type: 'wood', res: wood })
+    }
+    if (gold) {
+      priceChanged = true
+      setUnitResourceAttributes({ type: 'gold', res: gold })
+    }
+    if (stone) {
+      priceChanged = true
+      setUnitResourceAttributes({ type: 'stone', res: stone })
+    }
+  })
+
+  // reset timer on unit info
+  unitBox.querySelector('.time-cont div').innerText = isInt(tempTrainTime)
+    ? Math.ceil(tempTrainTime)
+    : tempTrainTime.toFixed(2)
+
+  if (!priceChanged) {
+    fwgs.map(setResToBaseValue)
+  }
+  updateUnitResourceInfo(unitStatsBox, unitBox)
+  unitStatsBox.setAttribute('x-train-time', tempTrainTime.toFixed(2))
+  unitCalc(unitStatsBox, 'recalc')
+  hideThingsIfUnitPriceIsZero(unitStatsBox, unitBox)
+}
+
+export const calculateBonusesOnUnit2 = (option) => (target) => {
   let { priceChanged, tempTrainTime } = Object.assign(
     {
       priceChanged: false,
