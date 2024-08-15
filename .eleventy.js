f1d3315737b8958b9352f0f1601d5a2a8dbe9391
Peter White <pedroblanco3257@gmail.com>
2024-08-15 11:52:02 +0900

    eleventy

--- .eleventy.js
+++ .eleventy.js
@@ -7,6 +7,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/**/*.jpg')
   eleventyConfig.addPassthroughCopy('src/**/*.css')
   eleventyConfig.addPassthroughCopy('src/**/*.js')
+  eleventyConfig.addPassthroughCopy('src/**/*.txt')
   return {
     dir: {
       input: 'src',
